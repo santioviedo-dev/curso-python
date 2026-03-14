581c110cf770a818cd8059a690e064ef1de34ebc
“santioviedo-dev” <oviedos412@gmail.com>
2026-03-14 11:28:54 -0300

    Add pad and ipad in grid

--- tkinter/05_grid.py
+++ tkinter/05_grid.py
@@ -14,15 +14,15 @@ btn3 = ttk.Button(window, text="Button 3")
 
 # Configurate columns
 window.columnconfigure(0, weight=1)
-window.columnconfigure(1, weight=10)
+window.columnconfigure(1, weight=1)
 window.columnconfigure(2, weight=1)
 window.rowconfigure(0, weight=1)
 window.rowconfigure(1, weight=1)
 window.rowconfigure(2, weight=1)
 # This set the space that occupies each column and row
 
-btn1.grid(row=0, column=0)
-btn2.grid(row=1, column=1)
-btn3.grid(row=2, column=2)
+btn1.grid(row=0, column=0, sticky=tk.NSEW) # set the button position inside ther box. In this case, is set to north.
+btn2.grid(row=0, column=1, ipadx=20)
+btn3.grid(row=0, column=2, pady=15, ipadx=5)
 
-window.mainloop() 
+window.mainloop()
